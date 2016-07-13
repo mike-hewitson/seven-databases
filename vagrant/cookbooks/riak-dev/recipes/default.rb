b2995a034952be20898e60af11f16588623f80cc
Mike Hewitson <mike@hewitson.co.za>
2016-07-13 12:34:53 +0200

    finalise the riak-dev installation script

--- vagrant/cookbooks/riak-dev/recipes/default.rb
+++ vagrant/cookbooks/riak-dev/recipes/default.rb
@@ -1,17 +1,20 @@
 bash "install_riak_dev" do
   code <<-EOH
+  		cd /home/vagrant
 		curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
 		chmod a+x kerl
 		sudo apt-get update
 		sudo apt-get -y install build-essential autoconf libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev git
 		sudo apt-get -y install libpam0g-dev
 		
+		cd /home/vagrant
 		wget http://s3.amazonaws.com/downloads.basho.com/erlang/otp_src_R16B02-basho8.tar.gz
 		tar zxvf otp_src_R16B02-basho8.tar.gz
 		cd OTP_R16B02_basho8
 		./otp_build autoconf
 		./configure && make && sudo make install
 
+		cd /home/vagrant
 		mkdir -p Projects/basho
 		cd Projects/basho
 		git clone git://github.com/basho/riak.git
