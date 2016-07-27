d714d8df69cd2f9b98f9da68a8c3d77ff26732bf
MusaNkosi <mcnkosi1@gmail.com>
2016-07-27 03:55:59 +0200

    Added scripts to update ruby to 2.0.0 and install riak-client json.

--- vagrant/cookbooks/riak-dev/recipes/default.rb
+++ vagrant/cookbooks/riak-dev/recipes/default.rb
@@ -22,5 +22,16 @@ bash "install_riak_dev" do
 		make locked-deps
 		make devrel DEVNODES=3
 
+    sudo apt-get -y update
+    sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+    cd /tmp
+    wget http://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz
+    tar -xvzf ruby-2.0.0-p481.tar.gz
+    cd ruby-2.0.0-p481/
+    ./configure --prefix=/usr/local
+    make
+    sudo make install
+    cd /home/vagrant/Projects/basho/riak
+    gem install riak-client json
   EOH
 end
