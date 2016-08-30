c05a0d6a7f8bc9ae73754c76373946e0a362baeb
Muneeb Baderoen <mbaderoen@gmail.com>
2016-08-31 00:01:36 +0200

    Add creation of vagrant user for postgres

--- vagrant/cookbooks/postgres/recipes/default.rb
+++ vagrant/cookbooks/postgres/recipes/default.rb
@@ -1,5 +1,15 @@
 bash "install_postgres_database" do
   code <<-EOH
-        sudo apt-get -y install postgresql-#{node['postgres']['version']}
+	sudo apt-get -y install postgresql-#{node['postgres']['version']}
   EOH
 end
+
+
+bash "create_vagrant_database" do
+  user "vagrant"
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})
+  code <<-EOH
+  	sudo -u postgres createuser -s $(whoami); createdb $(whoami)
+  EOH
+end
+
