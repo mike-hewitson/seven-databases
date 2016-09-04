7752c8b1d1b7f735e9ec74fc8398b6dd76bd6de1
Muneeb Baderoen <mbaderoen@gmail.com>
2016-09-04 17:02:42 +0200

    Add sed command to replace 127.0.0.1 with 0.0.0.0
    Allows cluster to be accessed from outside the vm using postman or any
    other http client from the host system.

--- vagrant/cookbooks/riak-dev/recipes/default.rb
+++ vagrant/cookbooks/riak-dev/recipes/default.rb
@@ -1,26 +1,29 @@
 bash "install_riak_dev" do
   code <<-EOH
-  		cd /home/vagrant
-		curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
-		chmod a+x kerl
-		sudo apt-get update
-		sudo apt-get -y install build-essential autoconf libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev git
-		sudo apt-get -y install libpam0g-dev
-		
-		cd /home/vagrant
-		wget http://s3.amazonaws.com/downloads.basho.com/erlang/otp_src_R16B02-basho8.tar.gz
-		tar zxvf otp_src_R16B02-basho8.tar.gz
-		cd OTP_R16B02_basho8
-		./otp_build autoconf
-		./configure && make && sudo make install
+    cd /home/vagrant
+    curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
+    chmod a+x kerl
+    sudo apt-get update
+    sudo apt-get -y install build-essential autoconf libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev git
+    sudo apt-get -y install libpam0g-dev
 
-		cd /home/vagrant
-		mkdir -p Projects/basho
-		cd Projects/basho
-		git clone git://github.com/basho/riak.git
-		cd riak
-		make locked-deps
-		make devrel DEVNODES=5
+    cd /home/vagrant
+    wget http://s3.amazonaws.com/downloads.basho.com/erlang/otp_src_R16B02-basho8.tar.gz
+    tar zxvf otp_src_R16B02-basho8.tar.gz
+    cd OTP_R16B02_basho8
+    ./otp_build autoconf
+    ./configure && make && sudo make install
+
+    cd /home/vagrant
+    mkdir -p Projects/basho
+    cd Projects/basho
+    git clone git://github.com/basho/riak.git
+    cd riak
+    make locked-deps
+    make devrel DEVNODES=5
+
+    cd dev
+    for node in dev*; do sudo sed -i.bak s/127.0.0.1/0.0.0.0/g $node/etc/riak.conf; done
 
     sudo apt-get -y update
     sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
