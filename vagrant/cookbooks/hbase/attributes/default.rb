c600434e50d2d485e5b3f20b0d2406f01c3b9ec6
robert passmore <robertmpassmore@gmail.com>
2016-09-24 12:13:55 +0200

    update hbase stable version number from 1.2.2 to 1.2.3, fixing chef recipe http 404 failure

--- vagrant/cookbooks/hbase/attributes/default.rb
+++ vagrant/cookbooks/hbase/attributes/default.rb
@@ -1,5 +1,5 @@
-default['hbase']['tar'] = "hbase-1.2.2-bin.tar.gz"
-default['hbase']['url'] = "http://apache.is.co.za/hbase/stable/hbase-1.2.2-bin.tar.gz"
+default['hbase']['tar'] = "hbase-1.2.3-bin.tar.gz"
+default['hbase']['url'] = "http://apache.is.co.za/hbase/stable/hbase-1.2.3-bin.tar.gz"
 default['hadoop']['tar'] = "hadoop-2.7.2.tar.gz"
 default['hadoop']['url']="http://apache.saix.net/hadoop/common/hadoop-2.7.2/hadoop-2.7.2.tar.gz"
 default['hbase']['user']="vagrant"
